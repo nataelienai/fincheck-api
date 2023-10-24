efaa4c6165b7bfa4d8a8adb6e59f20cfc6a5e857
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-10-24 20:57:35 -0300

    feat: implement create user route

--- src/users/users.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-
-@Injectable()
-export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return createUserDto;
-  }
-}
