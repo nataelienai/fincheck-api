3dedcbe138cb28c9c1af94491f3d4e904ded5b92
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-10-24 18:30:19 -0300

    feat: create prisma models and migrations

---
+++ prisma/migrations/20231024210417_create_bank_accounts_table/migration.sql
@@ -0,0 +1,17 @@
+-- CreateEnum
+CREATE TYPE "bank_account_type" AS ENUM ('CHECKING', 'INVESTMENT', 'CASH');
+
+-- CreateTable
+CREATE TABLE "bank_accounts" (
+    "id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "initial_balance" DOUBLE PRECISION NOT NULL,
+    "type" "bank_account_type" NOT NULL,
+    "color" TEXT NOT NULL,
+
+    CONSTRAINT "bank_accounts_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "bank_accounts" ADD CONSTRAINT "bank_accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
