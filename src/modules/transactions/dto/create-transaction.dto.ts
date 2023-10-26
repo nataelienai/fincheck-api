13c8a559232728f13638356c90b2aadb2403a614
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-10-26 19:55:44 -0300

    feat: implement route to create a transaction

--- src/modules/transactions/dto/create-transaction.dto.ts
+++ src/modules/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,39 @@
-export class CreateTransactionDto {}
+import { TransactionType } from '@prisma/client';
+import {
+  IsDateString,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  IsUUID,
+} from 'class-validator';
+
+export class CreateTransactionDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsUUID()
+  bankAccountId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsUUID()
+  categoryId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
+  value: number;
+
+  @IsNotEmpty()
+  @IsDateString()
+  date: string;
+
+  @IsNotEmpty()
+  @IsEnum(TransactionType)
+  type: TransactionType;
+}
--- src/modules/transactions/transactions.service.ts
+++ src/modules/transactions/transactions.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { TransactionsRepository } from 'src/shared/database/repositories/transactions.repository';
+import { BankAccountOwnershipValidationService } from '../bank-accounts/services/bank-account-ownership-validation.service';
+import { CategoryOwnershipValidationService } from '../categories/services/category-ownership-validation.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
 
@@ -7,10 +9,27 @@ import { UpdateTransactionDto } from './dto/update-transaction.dto';
 export class TransactionsService {
   constructor(
     private readonly transactionsRepository: TransactionsRepository,
+    private readonly bankAccountOwnershipValidationService: BankAccountOwnershipValidationService,
+    private readonly categoryOwnershipValidationService: CategoryOwnershipValidationService,
   ) {}
 
-  create(createTransactionDto: CreateTransactionDto) {
-    return createTransactionDto;
+  async create(userId: string, createTransactionDto: CreateTransactionDto) {
+    const { bankAccountId, categoryId, name, value, date, type } =
+      createTransactionDto;
+
+    await this.validateEntitiesOwnership({ userId, bankAccountId, categoryId });
+
+    return this.transactionsRepository.create({
+      data: {
+        userId,
+        bankAccountId,
+        categoryId,
+        name,
+        value,
+        date,
+        type,
+      },
+    });
   }
 
   findAllByUserId(userId: string) {
@@ -23,5 +42,23 @@ export class TransactionsService {
 
   remove(id: string) {
     return `This action removes a #${id} transaction`;
+  }
+
+  private async validateEntitiesOwnership({
+    userId,
+    categoryId,
+    bankAccountId,
+  }: {
+    userId: string;
+    categoryId: string;
+    bankAccountId: string;
+  }) {
+    await Promise.all([
+      this.bankAccountOwnershipValidationService.validate(
+        userId,
+        bankAccountId,
+      ),
+      this.categoryOwnershipValidationService.validate(userId, categoryId),
+    ]);
   }
 }
