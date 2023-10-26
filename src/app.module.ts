21931ba37b0a66199e8ea7140eec63a097af708d
Natã Elienai <68663436+nataelienai@users.noreply.github.com>
2023-10-26 16:03:06 -0300

    feat: add route to list categories for a user

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AuthGuard } from './modules/auth/auth.guard';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { DatabaseModule } from './shared/database/database.module';
+import { CategoriesModule } from './modules/categories/categories.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, AuthModule],
+  imports: [UsersModule, DatabaseModule, AuthModule, CategoriesModule],
   controllers: [],
   providers: [
     {
--- src/shared/database/database.module.ts
+++ src/shared/database/database.module.ts
@@ -1,10 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { CategoriesRepository } from './repositories/categories.repository';
 import { UsersRepository } from './repositories/users.repository';
 
 @Global()
 @Module({
-  providers: [PrismaService, UsersRepository],
-  exports: [UsersRepository],
+  providers: [PrismaService, UsersRepository, CategoriesRepository],
+  exports: [UsersRepository, CategoriesRepository],
 })
 export class DatabaseModule {}
